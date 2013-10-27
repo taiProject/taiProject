55b1a25d28afdaf0455f979c3604e3859e474dad
Aleksander <Aleksander@alek>
2013-10-27 17:43:43 +0100

    Updated junit version, added test configuration, added dropbox sdk, added liquibase configuration, updated i18n folder.

--- dfs/src/main/java/pl/edu/agh/dfs/controllers/DemoController.java
+++ dfs/src/main/java/pl/edu/agh/dfs/controllers/DemoController.java
@@ -1,14 +1,18 @@
 package pl.edu.agh.dfs.controllers;
 
+import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class DemoController {
+@Scope("session")
+@SuppressWarnings("serial")
+public class DemoController implements Serializable {
 
 	private final List<String> msgs = new LinkedList<String>();
 
